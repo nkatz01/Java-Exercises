7ee93cf9f2d2b1fefcb1fda971319ed2939e4959
nuchemka <nuchemka@gmail.com>
2019-09-02 12:54:38 +0100

    2017Q8

---
+++ previousExams/CashRegisterMK2.java
@@ -0,0 +1,58 @@
+package previousExams;
+import java.util.ArrayList; 
+import java.util.Iterator; 
+/**
+ * Write a description of class CashRegisterMK2 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class CashRegisterMK2
+{
+    // instance variables - replace the example below with your own
+    private ArrayList<Double> items;
+
+    /**
+     * Constructor for objects of class CashRegisterMK2
+     */
+    public CashRegisterMK2()
+    {
+        // initialise instance variables
+        items = new ArrayList<Double>();
+    }
+    
+    public void addItem(double price){
+        items.add(price);
+    }
+    
+    public void clear(){
+      // int n = items.size(); 
+        //for (Double item: items){
+           // for (int i=0; i<n; i++){
+           Iterator<Double> iter = items.iterator(); 
+           while (iter.hasNext()){
+           Double item =iter.next();
+            iter.remove(); }
+        
+    }
+    
+    public double getTotal(){
+        double price=0;
+        for(Double item: items){
+            price+=item; 
+        }
+        return price; 
+    }
+    
+    public int getCount(){
+    return items.size(); 
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    
+}
