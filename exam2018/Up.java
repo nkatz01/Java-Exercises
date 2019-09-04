08a683def8d8ca3ddc25489545ec0122f9cc2ff8
Nuchem Katz <nuchemka@gmail.com>
2019-09-04 14:22:24 +0100

    2018Q9

---
+++ exam2018/Up.java
@@ -0,0 +1,22 @@
+package exam2018;
+
+
+/**
+ * Write a description of class Up here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Up {
+private String name;
+public Up(String name) {
+this.name = name;
+}
+public void setName(String name) {
+name = this.name;
+}
+public String getName() {
+return name;
+}
+
+}
