2c1f39ce09993dd638a838a1613f0a44406be0f4
Nuchem Katz <nuchemka@gmail.com>
2019-09-05 21:18:39 +0100

    2016Q6 a) and b)

---
+++ exam2016/Tree.java
@@ -0,0 +1,81 @@
+package exam2016;
+import java.util.ArrayList; 
+
+/**
+ * Write a description of class dsfsf here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Tree {
+    private Tree left;
+    private Tree right;
+    public int value;
+
+public Tree(Tree left, Tree right, int value) {
+    
+    this.left = left;
+    this.right = right;
+    this.value = value;
+}
+
+public  ArrayList<Integer> asList(){
+    ArrayList<Integer> al = new ArrayList<>(); 
+    if (right==null && left==null){
+        al.add(value); 
+        return al ;
+    }
+    else if (left!=null && right==null){
+    al.addAll(left.asList()); 
+    al.add(value); 
+    return al; 
+
+   }
+   else if (left==null && right!=null){
+    al.addAll(right.asList()); 
+    al.add(value); 
+    return al; 
+    }
+    else{
+    al.addAll(left.asList()); 
+    al.addAll(right.asList());
+    al.add(value); 
+    return al;
+    }
+
+}
+ public Tree getMirrored(){
+    Tree tree; 
+    if (left==null && right==null){
+      //    tree = new Tree(left, right, value); 
+        return this; }
+    else  if (left!=null && right==null)
+       return new Tree(null, left.getMirrored() , value); 
+    else if (left==null && right!=null)
+     return new Tree(right.getMirrored(), null , value); 
+    else
+    return new Tree( right.getMirrored() , left.getMirrored() , value);
+        
+        
+    } 
+/*public int deleteLeavesWith(int val){
+    if (right==null & left == null){
+    if (value == val)
+   return this.value;
+   if left.deleteLeavesWith(val) == val
+}
+        
+}*/
+
+public Tree getRight(){
+
+return right; 
+}
+
+public Tree getLeft(){
+
+return left; 
+}
+
+
+}
---
+++ exam2016/TreeDr.java
@@ -0,0 +1,32 @@
+package exam2016;
+
+
+/**
+ * Write a description of class TreeDr here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class TreeDr
+{
+  public static void main(String[] args){
+    Tree minusOne = new Tree(null, null, -1); 
+    Tree eighteen = new Tree(minusOne, null, 18); 
+    Tree three = new Tree(null, null ,3); 
+    Tree fortyTwo = new Tree(three, eighteen, 42); 
+    Tree minusEight = new Tree(null, null, -8); 
+    Tree rootFifteen = new Tree(fortyTwo, minusEight, 15);
+   // System.out.println(minusEight.getLeft().value);
+   // System.out.println(minusEight.getRight().value);
+     System.out.println(rootFifteen.asList()); 
+     Tree treeMirrored = rootFifteen.getMirrored(); 
+     System.out.println(treeMirrored.asList());
+      System.out.println(treeMirrored.getLeft().value);
+   System.out.println(treeMirrored.getRight().value);
+  Tree mirFortyTwo = treeMirrored.getRight();
+  
+   Tree mirEighteen = mirFortyTwo.getLeft(); 
+  System.out.println(mirFortyTwo.getRight().value);
+   System.out.println(mirEighteen.getRight().value);
+    }
+}
