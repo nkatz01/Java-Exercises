5b7605224e62117c1f9d8c89b221809fbbef792a
nuchemka <nuchemka@gmail.com>
2019-09-11 17:39:08 +0100

    2016Q8a)

---
+++ exam2016/ElecStove.java
@@ -0,0 +1,27 @@
+package exam2016;
+
+
+/**
+ * Write a description of class ElecStove here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ElecStove extends Electric
+{
+    // instance variables - replace the example below with your own
+    private double diameter;
+
+    /**
+     * Constructor for objects of class ElecStove
+     */
+     
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+     
+}
---
+++ exam2016/Radiator.java
@@ -0,0 +1,27 @@
+package exam2016;
+
+
+/**
+ * Write a description of class Radiator here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Radiator extends Electric
+{
+    // instance variables - replace the example below with your own
+    private int fins;
+
+    /**
+     * Constructor for objects of class Radiator
+     */
+   
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    
+}
